c5ba5eb1d0f92beab6c3609383ea10566b0b561f
athamov <abdulloxathamoff@gmail.com>
2022-03-30 21:12:45 +0500

    done some changes

--- listener.js
+++ listener.js
@@ -1,4 +1,3 @@
-
 //! listeners 
 const play = document.querySelector('#play');
 const pause = document.querySelector('#pause');
@@ -7,19 +6,65 @@ play.addEventListener("click",()=>{
   play.style.visibility="hidden"
   pause.style.visibility="visible"
 
-  if(currentType==undefined) setType()
+  boardArray = createCleanArray()
+  trailShape = [beginX,0]
 
-  // Interval = setInterval(()=>{
-    
-  //   if(current.coordinateY==19*BLOCK_SIZE) stop()
-  // }, 1000);
+  currentType = nextType
+  nextType = types[nameOfTypes[randomNumber(6)]]
+  currentBlock = new Block(currentType.color,currentType.states[randomNumber(currentType.size)]);
+
+
+  Interval = setInterval(()=>{
+    checkPlace = checking_zero(trailShape[0] , trailShape[1], trailShape[0] + currentType.size, trailShape[1] + currentType.size)
+    console.log("Checking" + checkPlace)
+
+    if(checkPlace) {
+      currentBlock.cleanShape(trailShape[0]-1 , trailShape[1])
+      currentBlock.drawShape(trailShape[0], trailShape[1])
+    }
+
+
+    trailShape[1] += 1
+    // if(current.coordinateY==19*BLOCK_SIZE) stop()
+  }, 1000);
 })
 
 pause.addEventListener("click",()=>{
   pause.style.visibility="hidden"
   play.style.visibility="visible"
+
 })
 function stop() {
   console.log("stop")
   clearInterval(Interval);
 }
+
+
+
+
+//! keyboard function
+// function doKeyDown(evt){
+//   switch (evt.keyCode) {
+//   case 38:  /* Up arrow was pressed */
+//   if (y - dy > 0){
+//   y -= dy;
+//   }
+//   break;
+//   case 40:  /* Down arrow was pressed */
+//   if (y + dy < HEIGHT){
+//   y += dy;
+//   }
+//   break;
+//   case 37:  /* Left arrow was pressed */
+//   if (x - dx > 0){
+//   x -= dx;
+//   }
+//   break;
+//   case 39:  /* Right arrow was pressed */
+//   if (x + dx < WIDTH){
+//   x += dx;
+//   }
+//   break;
+//   }
+//   }
+// window.addEventListener('keydown',doKeyDown,true);
